2e7bfd71b63c087f324d383d62b6f096b0bce61d
Paulgflatlooker <paul.gravejal@flatlooker.com>
2023-12-04 16:46:39 +0100

    before devise

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -7,8 +7,38 @@ class MapsController < ApplicationController
     @map = Map.find(params[:id])
   end
 
+  def new
+    @map = Map.new
+  end
+
+  def destroy
+    @map = Map.find(params[:id])
+    @map.destroy
+
+    redirect_to root_path, status: :see_other
+  end
+
+  def create_simple
+    @map = Map.new(map_params)
+    rows = 10 # Replace with the desired number of rows
+    cols = 10 # Replace with the desired number of columns
+    num_mines = 10 # Replace with the desired number of mines
+    @map.generate_minesweeper_map(rows, cols, num_mines)
+    if @map.save
+      redirect_to @map, notice: 'Have Fun !'
+    else
+      render json: @map.errors, status: :unprocessable_entity
+    end
+  end
+
   def map_to_json
     @map = Map.find(params[:id])
     render json: @map
+  end
+
+  private
+
+  def map_params
+    params.require(:map).permit(:name)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root 'maps#index'
 
   resources :maps
+  post '/maps/create_simple', to: 'maps#create_simple', as: 'create_simple_map'
   get '/maps/:id', to: 'maps#show'
   get '/maps/:id/map_to_json', to: 'maps#map_to_json'
 end
