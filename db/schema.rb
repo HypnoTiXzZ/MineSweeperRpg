63f2d487f2b8f4a45b9445011b5dbaf9cfad4f0a
Paulgflatlooker <paul.gravejal@flatlooker.com>
2023-12-09 14:01:08 +0100

    before adding username

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_12_05_130804) do
+ActiveRecord::Schema[7.0].define(version: 2023_12_08_192541) do
   create_table "maps", force: :cascade do |t|
     t.string "name"
     t.json "tiles"
@@ -28,6 +28,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_12_05_130804) do
     t.datetime "updated_at", null: false
     t.integer "balance"
     t.integer "map_id"
+    t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
